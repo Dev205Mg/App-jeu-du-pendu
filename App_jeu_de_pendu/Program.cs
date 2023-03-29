afb41c260ce5644dd87f1ca5e3b1afdbc4d5c99e
Dev205 <rodyael.205@gmail.com>
2023-03-29 11:13:46 +0300

    Add project files.

---
+++ App_jeu_de_pendu/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_jeu_de_pendu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
